a20fd742e637373c3b2422e5aae9f10a01dcf679
Nur-Aimanecho <nur_aiman71099@yahoo.com>
2023-07-13 11:55:39 +0800

    fix timezone

--- src/pages/MyAppointment.jsx
+++ src/pages/MyAppointment.jsx
@@ -294,6 +294,7 @@ function MyAppointment() {
                         day: '2-digit',
                         hour: '2-digit',
                         minute: '2-digit',
+                        timeZone: 'UTC',
                       }
                     ).format(new Date(currentSessionData.date_time))}
                   `
